53960739cb9a44769e07d4ca7ee6731303b92a36
Shri-poorna <shripoornajbhat@gmail.com>
2026-04-20 20:09:19 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+num1=float(input("Enter first no: "))
+num2=float(input("Enter second no: "))
+sum1=num1+num2
+dif1=num1-num2
+prd1=num1*num2
+if(num2!=0):
+    div1=num1/num2
+    div2=num1//num2
+    rem1=num1%num2
+    exp1=num1**num2
+print(sum1,dif1,prd1,div1,div2,rem1,exp1)
